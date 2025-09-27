49d16ba7e918adbf4e4e935649b8e475a7818016
Tarun Sharma <tarundadhich05@gmail.com>
2025-09-27 17:44:34 +0530

    build: Update Gradle configuration and dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,11 +3,13 @@ plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.kotlin.compose) apply false
-    id("com.google.devtools.ksp") version "2.2.0-2.0.2" apply false
-    id("com.google.dagger.hilt.android") version "2.56.2" apply false
-    id("com.google.gms.google-services") version "4.4.2" apply false
-    id("org.jetbrains.kotlin.plugin.serialization") version "2.2.0" apply false
+    id("com.google.devtools.ksp") version "2.2.20-2.0.3" apply false
+    id("com.google.dagger.hilt.android") version "2.57.1" apply false
+    id("com.google.gms.google-services") version "4.4.3" apply false
+    id("org.jetbrains.kotlin.plugin.serialization") version "2.2.20" apply false
+
 
 }
 val defaultTargetSdkVersion by extra(35)
 val buildToolsVersion by extra("35.0.0")
+val buildToolsVersion1 by extra(buildToolsVersion)
